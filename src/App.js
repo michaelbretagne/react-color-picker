75abfba15cf7485b0707f61ae621d13b200445a8
Michael <michael.bretagne@gmail.com>
2019-07-07 11:35:04 -0700

    Add react-router-dom and palette routes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,31 @@
-import React from "react";
-import seedPalettes from "./seedPalettes";
+import React, { Component } from "react";
+import { Route, Switch } from "react-router-dom";
 import Palette from "./Palette";
+import seedPalettes from "./seedPalettes";
 import { generatePalette } from "./colorHelpers";
 
-function App() {
-  return <Palette palette={generatePalette(seedPalettes[1])} />;
+class App extends Component {
+  findPalette(id) {
+    return seedPalettes.find(palette => palette.id === id);
+  }
+
+  render() {
+    return (
+      <Switch>
+        <Route
+          exact
+          path="/palette/:id"
+          render={routeProps => (
+            <Palette
+              palette={generatePalette(
+                this.findPalette(routeProps.match.params.id),
+              )}
+            />
+          )}
+        />
+      </Switch>
+    );
+  }
 }
 
 export default App;
