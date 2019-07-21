edca0217e0caf2abbe6b45e210ab4a0cb103bc5f
Michael <michael.bretagne@gmail.com>
2019-07-20 19:03:37 -0700

    Add bg svg to PaletteList and animate palette deletion

--- src/styles/PaletteListStyles.js
+++ src/styles/PaletteListStyles.js
@@ -1,11 +1,28 @@
+import sizes from "./sizes";
+import bg from "./bg.svg";
+
 export default {
+  "@global": {
+    ".fade-exit": {
+      opacity: 1
+    },
+    ".fade-exit-active": {
+      opacity: 0,
+      transition: "opacity 500ms ease-out"
+    }
+  },
   root: {
-    backgroundColor: "blue",
-    height: "100vh",
+    height: "101vh",
     display: "flex",
     alignItems: "flex-start",
-    justifyContent: "center",
     overflowY: "scroll",
+    justifyContent: "center",
+    /* background by SVGBackgrounds.com */
+    backgroundColor: "#394bad",
+    backgroundImage: `url(${bg})`,
+  },
+  heading: {
+    fontSize: "2rem",
   },
   container: {
     width: "50%",
@@ -13,6 +30,12 @@ export default {
     alignItems: "flex-start",
     flexDirection: "column",
     flexWrap: "wrap",
+    [sizes.down("xl")]: {
+      width: "80%",
+    },
+    [sizes.down("xs")]: {
+      width: "75%",
+    },
   },
   nav: {
     display: "flex",
@@ -31,7 +54,13 @@ export default {
     width: "100%",
     display: "grid",
     gridTemplateColumns: "repeat(3, 30%)",
-    gridGap: "5%",
-    paddingBottom: "40px",
+    gridGap: "2.5rem",
+    [sizes.down("md")]: {
+      gridTemplateColumns: "repeat(2, 50%)",
+    },
+    [sizes.down("xs")]: {
+      gridTemplateColumns: "repeat(1, 100%)",
+      gridGap: "1.4rem",
+    },
   },
 };
